98ac4e4d0f2ecfd24ad9afb5fe294606d0c94494
Fabio Quintal <fabioamquintal@gmail.com>
2022-04-14 15:59:38 -0300

    exercise 7

--- http-client.js
+++ http-client.js
@@ -1,7 +1,8 @@
 const http = require('http')
-const url = process.argv[2].toString()
+const url = process.argv[2];
 
-http.get(url, (response) => {
+http.get(url, response => {
 	response.setEncoding('utf8')
-	response.on('data', (x) => console.log(x));
-});
+	response.on('data', (x) => console.log(x))
+	response.on("error", () => console.error)
+})
